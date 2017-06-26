8ed7843d83b7305a648f59cb252c34a22ed7090a
David Kao <dkao@assuredguaranty.com>
2017-06-26 16:55:17 -0400

    updates

---
+++ Database1/Scripts/SMKT.sql
@@ -0,0 +1,144 @@
+﻿SELECT count(s.ad_user) 
+FROM security_access a, permissions p, user_roles_ad_vw v, user_group_staff_vw s 
+WHERE a.app_name = p.programs and a.access_level = p.access_level and p.id in (v.role_1, v.role_2, v.role_3, v.role_4) 
+and s.ad_group = v.user_logid 
+and a.app_name='smkt' 
+and s.ad_user='dka' 
+and a.object_path='w_smkt_endors.m_smkt_endors.m_edit.m_update'
+
+/* SMKT Queue related 
+select *
+from ms_ee_queue
+order by post_dt desc
+
+select convert(date, post_dt), count(1)
+from ms_ee_queue
+group by convert(date, post_dt)
+order by 2 desc
+
+select distinct(user_id)
+from ms_ee_queue
+where user_id like 'agl\%'
+*/
+
+/* SMKT Security related query
+select top 1 1
+--SELECT *
+--SELECT COUNT(1)
+FROM [das].[dbo].[user_group_staff_vw]
+WHERE 
+--ad_user = 'dka'
+--and 
+ad_group in ( 'APP_ONL_smkt_2_edit2', 'APP_ONL_smkt_3_legal2', 'APP_ONL_smkt_4_ops2')
+
+  APP_ONL_Dev_User
+select v.*
+select distinct(user_logid)
+FROM security_access a, permissions p, user_roles_ad_vw v 
+WHERE a.app_name = p.programs and a.access_level = p.access_level and p.id in (v.role_1, v.role_2, v.role_3, v.role_4) 
+and a.app_name='smkt' 
+--and v.current_member = 1 
+and a.object_path='w_smkt_endors.m_smkt_endors.m_edit.m_update'
+
+select v.*
+--select count(1)
+--select distinct(user_logid)
+FROM security_access a, permissions p, user_roles_ad_vw v 
+WHERE a.app_name = p.programs and a.access_level = p.access_level and p.id in (v.role_1, v.role_2, v.role_3, v.role_4) 
+and a.app_name='smkt' 
+--and v.current_member = 1 
+and a.object_path='w_smkt_endors.m_smkt_endors.m_edit.m_update'
+
+select *
+from ms_ee_queue
+
+
+SELECT cn as ad_group, distinguishedName as dist_name
+	FROM OPENQUERY (ADSI, '<LDAP://dc=agl,dc=com>;(&(objectClass=group)(cn=*APP_ONL*));cn, distinguishedName;subtree')
+
+
+select sAMAccountName as ad_user
+from openquery(ADSI, '<LDAP://dc=agl,dc=com>;(&(objectCategory=person)(objectClass=user)(memberOf=CN='',OU=ACL Groups,OU=AGL Groups,DC=agl,DC=com));sAMAccountName;subtree')
+
+select staff_fstnm , *
+from fsa_staff
+
+select *
+from user_group_staff_vw
+where ad_user = 'dka'
+
+SELECT        b.init AS ad_user, b.staff_fstnm AS ad_group, b.staff_lstnm AS init, b.staff_ext AS staff_fstnm, b.staff_addr AS staff_lstnm, b.staff_city AS staff_ext, 
+                         b.staff_zip AS staff_addr, b.staff_spouse_nm AS staff_city, b.dept_cd AS staff_zip, b.pers_stat AS staff_spouse_nm, b.staff_st AS dept_cd, b.staff_phone AS pers_stat,
+                          b.ip_address AS staff_st, b.fsa_site AS staff_phone, b.staff_function AS ip_address, b.staff_title AS fsa_site, b.staff_title2 AS staff_function, b.email AS staff_title, 
+                         b.prefix AS staff_title2, b.staff_fax AS email, b.team_cd AS prefix, b.edms_dt AS staff_fax, b.active_directory_login AS team_cd, b.domain_controller AS edms_dt, 
+                         b.round_robin AS active_directory_login, b.exclude_round_robin_flg AS domain_controller, b.sid AS round_robin, b.current_login AS exclude_round_robin_flg, 
+                         b.SysStartTime AS sid, b.SysEndTime AS current_login, a.ad_user AS SysStartTime, a.ad_group AS SysEndTime
+FROM            OPENQUERY([LOCAL], 'exec [das].dbo.ad_user_groups') AS a LEFT OUTER JOIN
+                         dbo.fsa_staff AS b ON a.ad_user = b.active_directory_login
+
+where ad_user = 'dka'
+
+select IS_MEMBER('AGL\APP_ONL_Dev_User')
+select IS_MEMBER('AGL\APP_ONL_smkt_2_edit')
+/*
+[PrincipalPermission(SecurityAction.Demand, Role = "APP_ONL_smkt_2_edit")]
+        [PrincipalPermission(SecurityAction.Demand, Role = "APP_ONL_smkt_3_legal")]
+        [PrincipalPermission(SecurityAction.Demand, Role = "APP_ONL_smkt_4_ops")]
+		*/
+
+IF IS_ROLEMEMBER ('db_datareader') = 1  
+   print 'Current user is a member of the db_datareader role'  
+ELSE IF IS_ROLEMEMBER ('db_datareader') = 0  
+   print 'Current user is NOT a member of the db_datareader role'  
+ELSE IF IS_ROLEMEMBER ('db_datareader') IS NULL  
+   print 'ERROR: The database role specified is not valid.';  
+
+select IS_ROLEMEMBER('fsa_online_read_only_role', 'agl\dka')
+select IS_ROLEMEMBER('APP_ONL_Dev_User')
+select IS_ROLEMEMBER('db_datareader', 'agl\llasek')
+select IS_ROLEMEMBER('smkt_entry_role', 'agl\llasek')
+select IS_ROLEMEMBER('smkt_entry_role', 'agl\dka')
+llasek 
+fsa_online_read_only_role
+
+smkt_entry_role
+fsa_online_read_only_role
+fsa_online_role
+
+SELECT SUSER_ID('sa'); 
+
+SELECT r.name 
+  FROM sys.server_role_members AS m
+  INNER JOIN sys.server_principals AS l
+    ON m.member_principal_id = l.principal_id
+  INNER JOIN sys.server_principals AS r
+    ON m.role_principal_id = r.principal_id
+  WHERE l.name = N'agl\dka';
+
+
+SELECT g.[name], p.[name] 
+FROM sys.database_principals g inner join sys.database_role_members r on g.principal_id = r.role_principal_id
+inner join sys.database_principals p on p.principal_id = r.member_principal_id 
+WHERE (g.[type] = 'R') and (p.[type] = 'G' OR p.type = 'U') and p.name like '%APP_ONL%' and g.name not like 'db_%' and g.name not like 'test%';
+APP_ONL_Dev_User
+ select *
+ from sys.database_principals 
+ where type in ('G','U')
+
+ select *
+ from sys.database_role_members
+
+
+ SELECT DP1.name AS DatabaseRoleName,   
+   isnull (DP2.name, 'No members') AS DatabaseUserName   
+ FROM sys.database_role_members AS DRM  
+ RIGHT OUTER JOIN sys.database_principals AS DP1  
+   ON DRM.role_principal_id = DP1.principal_id  
+ LEFT OUTER JOIN sys.database_principals AS DP2  
+   ON DRM.member_principal_id = DP2.principal_id  
+WHERE DP1.type = 'R'
+--and DP1.name = 'db_owner'
+--and DP1.name like '%smkt%'
+and DP2.name like '%APP_ONL_Dev%'
+order by 1
+*/
---
+++ Database1/Scripts/Temporal.sql
@@ -0,0 +1,234 @@
+﻿UPDATE history.pf_issue_maturities 
+SET SysStartTime = DATEADD(hh,CASE WHEN SysStartTime >= dbo.GetDstStart(YEAR(SysStartTime)) AND SysStartTime < dbo.GetDstEnd(YEAR(SysStartTime)) THEN 4 ELSE 5 END,SysStartTime)
+,SysEndTime = DATEADD(hh,CASE WHEN SysEndTime >= dbo.GetDstStart(YEAR(SysEndTime)) AND SysEndTime < dbo.GetDstEnd(YEAR(SysEndTime)) THEN 4 ELSE 5 END,SysEndTime);
+go
+
+ALTER TABLE dbo.pf_issue_maturities DROP PERIOD FOR SYSTEM_TIME;
+go
+
+--UPDATE dbo.pf_issue_maturities SET SysStartTime = c.SysEndTime
+--FROM history.pf_issue_maturities a INNER JOIN dbo.pf_issue_maturities c
+--ON a.sp_record_id = c.sp_record_id AND a.entity_id = c.entity_id
+--INNER JOIN (SELECT sp_record_id,entity_id,MAX(SysEndTime)et FROM history.pf_issue_maturities GROUP BY sp_record_id,entity_id)b
+--ON a.sp_record_id = b.sp_record_id AND a.entity_id = b.entity_id;
+--UPDATE dbo.pf_issue_maturities SET SysStartTime = b.et
+--FROM dbo.pf_issue_maturities a INNER JOIN 
+--(SELECT sp_record_id,entity_id,MAX(SysEndTime)et FROM history.pf_issue_maturities GROUP BY sp_record_id,entity_id) b
+--ON a.sp_record_id = b.sp_record_id AND a.entity_id = b.entity_id;
+
+UPDATE dbo.pf_issue_maturities SET SysStartTime = b.SysStartTime
+FROM dbo.pf_issue_maturities a INNER JOIN history.pf_issue_maturities b
+ON a.sp_record_id = b.sp_record_id AND a.entity_id = b.entity_id
+WHERE YEAR(b.SysEndTime) = 9999
+go
+
+DELETE FROM history.pf_issue_maturities WHERE YEAR(SysEndTime) = 9999
+go
+
+ALTER TABLE dbo.pf_issue_maturities ADD PERIOD FOR SYSTEM_TIME(sysstartTime, sysendtime);
+go
+
+ALTER TABLE dbo.pf_issue_maturities SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE = history.pf_issue_maturities, DATA_CONSISTENCY_CHECK=ON));
+go
+-----------
+
+
+select sp_record_id, entity_id, SysStartTime, SysEndTime, 1
+from pf_issue_maturities
+union
+select sp_record_id, entity_id, SysStartTime, SysEndTime, 0
+from history.pf_issue_maturities
+order by 1, 2, 3 desc,4 desc
+
+
+--select count(1)
+--from history.pf_issue_maturities 
+
+--UPDATE dbo.pf_issue_maturities SET SysStartTime = b.et
+select SysStartTime, SysEndTime, b.et
+FROM dbo.pf_issue_maturities a INNER JOIN 
+(SELECT sp_record_id,entity_id,MAX(SysEndTime)et FROM history.pf_issue_maturities GROUP BY sp_record_id,entity_id) b
+ON a.sp_record_id = b.sp_record_id AND a.entity_id = b.entity_id;
+
+select a.SysStartTime, a.SysEndTime, b.SysStartTime, b.SysEndTime
+FROM dbo.pf_issue_maturities a INNER JOIN history.pf_issue_maturities b
+ON a.sp_record_id = b.sp_record_id AND a.entity_id = b.entity_id
+WHERE year(b.SysEndTime) = 9999 and a.SysStartTime != b.SysStartTime
+
+--SELECT * INTO _p from history.pf_issue_maturities
+--SELECT * INTO _p2 from history.pf_issue_maturities
+
+-- select count(1) from history.pf_issue_maturities
+
+--INSERT INTO history.pf_issue_maturities select * from dbo._p2
+-- select * from dbo. pf_issue_maturities where YEAR(h_end_date) != 9999
+-- TRUNCATE TABLE history.pf_issue_maturities
+
+
+--select * into _pf from dbo.pf_issue_maturities
+
+UPDATE dbo.pf_issue_maturities SET SysStartTime = das.dbo.ToUTC(SysStartTime)
+
+
+------------------
+UPDATE dbo.pf_issue_maturities SET SysStartTime = das.dbo.ToUTC(SysStartTime);
+
+UPDATE history.pf_issue_maturities SET SysEndTime = c.SysStartTime
+FROM history.pf_issue_maturities a INNER JOIN pf_issue_maturities c
+ON a.sp_record_id = c.sp_record_id AND a.entity_id = c.entity_id
+INNER JOIN (SELECT sp_record_id,entity_id,MAX(SysStartTime)st, MAX(SysEndTime)et FROM history.pf_issue_maturities GROUP BY sp_record_id,entity_id)b
+ON a.sp_record_id = b.sp_record_id AND a.entity_id = b.entity_id AND a.SysStartTime = b.st AND a.SysEndTime = b.et;
+
+ALTER TABLE dbo.pf_issue_maturities ADD PERIOD FOR SYSTEM_TIME(sysstartTime, sysendtime);
+
+ALTER TABLE dbo.pf_issue_maturities SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE = history.pf_issue_maturities, DATA_CONSISTENCY_CHECK=ON));
+--------------------
+
+UPDATE dbo.setamper SET SysStartTime = das.dbo.ToUTC(SysStartTime);
+
+DECLARE @t Datetime2;
+SELECT @t = MAX(SysStartTime) FROM setamper;
+UPDATE history.setamper SET SysEndTime = @t
+FROM history.setamper a
+WHERE a.SysEndTime = (SELECT MAX(b.SysEndTime) FROM history.setamper b);
+
+ALTER TABLE dbo.setamper ADD PERIOD FOR SYSTEM_TIME(sysstartTime, sysendtime);
+
+ALTER TABLE dbo.setamper SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE=history.[setamper]));
+
+-------------------------------------------
+/*
+select a.*, (case when b.h_begin_date is null then getdate() else b.h_begin_date end) SysStartTime,'9999-12-31 23:59:59.999' SysEndTime 
+from dbo.risk a left join h_das.dbo.risk b
+on a.risk_no = b.risk_no and year(b.h_end_date) = 9999
+*/
+ALTER TABLE dbo.risk SET (SYSTEM_VERSIONING = OFF); 
+go
+
+ALTER TABLE dbo.risk DROP PERIOD FOR SYSTEM_TIME;
+go
+
+UPDATE dbo.risk SET SysStartTime = das.dbo.ToUTC(SysStartTime);
+go
+
+UPDATE history.risk SET SysEndTime = c.SysStartTime
+FROM history.risk a INNER JOIN dbo.risk c
+ON a.risk_no = c.risk_no
+INNER JOIN (SELECT risk_no, MAX(SysStartTime)st, MAX(SysEndTime)se
+FROM history.risk GROUP BY risk_no) b
+ON a.risk_no = b.risk_no AND a.SysStartTime = b.st AND a.SysEndTime = b.se;
+go
+
+ALTER TABLE dbo.risk ADD PERIOD FOR SYSTEM_TIME(sysstartTime, sysendtime);
+go
+
+ALTER TABLE dbo.risk SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE=history.risk, DATA_CONSISTENCY_CHECK=ON));
+go
+-------------------------------------------------------------------------
+/*
+select a.*,(case when b.h_begin_date is null then getdate() else b.h_begin_date end) SysStartTime,'9999-12-31 23:59:59.999' SysEndTime
+from dbo.stream_risk_free_rates a left join (select stream_no, max(h_begin_date) h_begin_date from h_das.dbo.stream_risk_free_rates where year(h_end_date) = 9999 group by stream_no) b
+on a.stream_no = b.stream_no 
+*/
+ALTER TABLE dbo.stream_risk_free_rates SET (SYSTEM_VERSIONING = OFF); 
+go
+
+ALTER TABLE dbo.stream_risk_free_rates DROP PERIOD FOR SYSTEM_TIME;
+go
+
+UPDATE dbo.stream_risk_free_rates SET SysStartTime = das.dbo.ToUTC(SysStartTime);
+go
+
+ALTER TABLE dbo.stream_risk_free_rates ADD PERIOD FOR SYSTEM_TIME(sysstartTime, sysendtime);
+go
+
+ALTER TABLE dbo.stream_risk_free_rates SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE=history.stream_risk_free_rates, DATA_CONSISTENCY_CHECK=ON));
+go
+-------------------------------------------------------------------------------
+ALTER TABLE dbo.risk_rtg_transfer SET (SYSTEM_VERSIONING = OFF); 
+go
+
+ALTER TABLE dbo.risk_rtg_transfer DROP PERIOD FOR SYSTEM_TIME;
+go
+
+UPDATE dbo.risk_rtg_transfer SET SysStartTime = das.dbo.ToUTC(SysStartTime);
+go
+
+/*
+select a.*, (case when b.h_begin_date is null then getdate() else b.h_begin_date end) SysStartTime,'9999-12-31 23:59:59.999' SysEndTime
+from dbo.risk_rtg_transfer a left join h_das.dbo.risk_rtg_transfer b
+on a.risk_no = b.risk_no and a.log_dt = b.log_dt and year(b.h_end_date) = 9999
+*/
+
+ALTER TABLE dbo.risk_rtg_transfer ADD PERIOD FOR SYSTEM_TIME(sysstartTime, sysendtime);
+go
+
+ALTER TABLE dbo.risk_rtg_transfer SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE = history.risk_rtg_transfer, DATA_CONSISTENCY_CHECK=ON));
+go
+----------------------------------------------------------------------------------------
+
+ALTER TABLE dbo.mrds_entity_risk SET (SYSTEM_VERSIONING = OFF); 
+go
+
+ALTER TABLE dbo.mrds_entity_risk DROP PERIOD FOR SYSTEM_TIME;
+go
+
+/*
+select a.*, (case when b.h_begin_date is null then getdate() else b.h_begin_date end) SysStartTime,'9999-12-31 23:59:59.999' SysEndTime
+from dbo.mrds_entity_risk a left join h_das.dbo.mrds_entity_risk b
+ON a.md_issuer_no = b.md_issuer_no AND a.debt_class_key = b.debt_class_key AND a.seniority_key = b.seniority_key AND a.risk_no = b.risk_no AND a.link_type = b.link_type
+and year(b.h_end_date) = 9999
+*/
+
+UPDATE dbo.mrds_entity_risk SET SysStartTime = das.dbo.ToUTC(SysStartTime);
+go
+
+ALTER TABLE dbo.mrds_entity_risk ADD PERIOD FOR SYSTEM_TIME(sysstartTime, sysendtime);
+go
+
+ALTER TABLE dbo.mrds_entity_risk SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE = history.mrds_entity_risk, DATA_CONSISTENCY_CHECK=ON));
+go
+-----------------------------------------------------------------
+ALTER TABLE dbo.mrds SET (SYSTEM_VERSIONING = OFF); 
+go
+
+ALTER TABLE dbo.mrds DROP PERIOD FOR SYSTEM_TIME;
+go
+
+/*
+select a.*,(case when b.h_begin_date is null then getdate() else b.h_begin_date end) SysStartTime,'9999-12-31 23:59:59.999' SysEndTime
+from dbo.mrds a left join (select md_unique_no, batch_number, max(h_begin_date) h_begin_date from h_das.dbo.mrds where year(h_end_date) = 9999 group by md_unique_no, batch_number) b
+on a.md_unique_no = b.md_unique_no AND a.batch_number = b.batch_number
+*/
+
+UPDATE dbo.mrds SET SysStartTime = das.dbo.ToUTC(SysStartTime);
+go
+
+ALTER TABLE dbo.mrds ADD PERIOD FOR SYSTEM_TIME(sysstartTime, sysendtime);
+go
+
+ALTER TABLE dbo.mrds SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE = history.mrds, DATA_CONSISTENCY_CHECK=ON));
+go
+--------------------------------------------------------
+ALTER TABLE dbo.pf_entity_risk SET (SYSTEM_VERSIONING = OFF); 
+go
+
+ALTER TABLE dbo.pf_entity_risk DROP PERIOD FOR SYSTEM_TIME;
+go
+
+/*
+select a.*,(case when b.h_begin_date is null then getdate() else b.h_begin_date end) SysStartTime,'9999-12-31 23:59:59.999' SysEndTime
+from dbo.pf_entity_risk a left join 
+(select entity_id, municipal_category_code, municipal_security_code, risk_no,link_type, max(h_begin_date) h_begin_date from h_das.dbo.pf_entity_risk where year(h_end_date) = 9999 
+group by entity_id, municipal_category_code, municipal_security_code, risk_no,link_type) b
+ON a.entity_id = b.entity_id AND a.municipal_category_code = b.municipal_category_code AND a.municipal_security_code = b.municipal_security_code 
+AND a.risk_no = b.risk_no AND a.link_type = b.link_type;
+*/
+UPDATE dbo.pf_entity_risk SET SysStartTime = das.dbo.ToUTC(SysStartTime);
+go
+
+ALTER TABLE dbo.pf_entity_risk ADD PERIOD FOR SYSTEM_TIME(sysstartTime, sysendtime);
+go
+
+ALTER TABLE dbo.pf_entity_risk SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE = history.pf_entity_risk, DATA_CONSISTENCY_CHECK=ON));
+go
