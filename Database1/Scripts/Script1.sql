a999cb95c78ef389fe581e255f826155afb262de
sz1989 <sz1989@gmail.com>
2021-02-24 17:45:42 -0500

    update

--- Database1/Scripts/Script1.sql
+++ Database1/Scripts/Script1.sql
@@ -1,3 +1,1 @@
-https://prod.liveshare.vsengsaas.visualstudio.com/join?F658FBD9BEEDB1B766FA2A4D588B45181B28
-
-https://prod.liveshare.vsengsaas.visualstudio.com/join?06EC3E11966CD57840F72FC10EA564B4AE93
+https://prod.liveshare.vsengsaas.visualstudio.com/join?0BEBDF50F20C622D4B873EAA748582F93352
