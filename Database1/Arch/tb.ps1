35c108b894f0a7f766c8f0b08b7b5aa3fd21d88f
sz1989 <sz1989@gmail.com>
2024-01-03 09:42:30 -0500

    Update tb.ps1

--- Database1/Arch/tb.ps1
+++ Database1/Arch/tb.ps1
@@ -19,16 +19,3 @@ if ($srv.ServerType-eq $null) # if it managed to find a server
 } 
 $scripter = new-object ("$My.Scripter") $srv # create the scripter
 $scripter.Options.ToFileOnly = $true 
-
-
-I can confirm that I can reproduce the user’s UAT issue last Thursday in the Development environment. The issue occurred after the user clicked on the 'To Next Month' button, and experienced a delay of more than a few seconds for the Data Validation service call to the server to complete. The delay may have been caused by the recent transition to cloud-based data storage. Despite the delay, the 'To New Month' button remained clickable, which may have caused the user to click on it multiple times, generating multiple click events. This, in turn, caused the service to move the Loss Period forward more than once.
-
-The Loss Calculator is designed to run one instance per server. Once the Loss Calculator runs in the “move Loss to the new period” mode, any further incoming requests to the Loss Calculator service will result in an error message stating that the Loss Calculator is unavailable (error code: 503). So, despite forwarding the Loss Period more than once, the Loss Calculator only calculated it once. 
-
-To avoid such an issue in the future, I suggest updating the code to disable the ‘To Next Month’ button immediately after the user clicks it. So, the Loss Home page does not have to wait for the Loss Calculator to start running on the server and send a signal to disable all buttons on the Loss Home page. This solution will address the UAT issue that occurred last Thursday. 
-
-Thanks,
-David Kao
-
-
-By disabling the button after the user clicks, we can easily solve this problem. It's a simple solution that will prevent any further issues down the line.
