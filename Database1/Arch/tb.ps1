22a7dafaab399603e987856e284f3b5bc5b4d9a0
sz1989 <sz1989@gmail.com>
2024-01-02 22:40:44 -0500

    Update tb.ps1

--- Database1/Arch/tb.ps1
+++ Database1/Arch/tb.ps1
@@ -21,11 +21,11 @@ $scripter = new-object ("$My.Scripter") $srv # create the scripter
 $scripter.Options.ToFileOnly = $true 
 
 
-I believe I can reproduce Andy's UAT issue last Thursday in the Development environment. After the user clicked the ‘To Next Month’ button, there was a delay of more than a few seconds for the Data Validation service call to complete (the delay was likely due to the recent transition to cloud-based data storage). However, the ‘To New Month’ button remained clickable. The user might have clicked on it more than once, causing the browser to generate multiple click events. This, in turn, triggered the service to move the Loss Period forward more than once.
+I can confirm that I can reproduce the user’s UAT issue last Thursday in the Development environment. The issue occurred after the user clicked on the 'To Next Month' button, and experienced a delay of more than a few seconds for the Data Validation service call to the server to complete. The delay may have been caused by the recent transition to cloud-based data storage. Despite the delay, the 'To New Month' button remained clickable, which may have caused the user to click on it multiple times, generating multiple click events. This, in turn, caused the service to move the Loss Period forward more than once.
 
-On the other hand, the Loss Calculator is designed to run one instance per server. Once the Loss Calculator runs in the “move Loss to the new period” mode, any further incoming requests to the Loss Calculator service will result in an error message stating that the Loss Calculator is unavailable (error code: 503). This was why, even though the Loss period had been forwarded more than once, the Loss Calculator calculated only once. 
+The Loss Calculator is designed to run one instance per server. Once the Loss Calculator runs in the “move Loss to the new period” mode, any further incoming requests to the Loss Calculator service will result in an error message stating that the Loss Calculator is unavailable (error code: 503). So, despite forwarding the Loss Period more than once, the Loss Calculator only calculated it once. 
 
-I suggest once the user clicks on the 'To Next Month' button, it should be immediately disabled. There will be no need to wait for the Loss Calculator to start running on the server and send a signal through SignalR to disable all buttons on the Loss Home page. This straightforward approach will effectively resolve the issue.
+To avoid such an issue in the future, I suggest updating the code to disable the ‘To Next Month’ button immediately after the user clicks it. So, the Loss Home page does not have to wait for the Loss Calculator to start running on the server and send a signal to disable all buttons on the Loss Home page. This solution will address the UAT issue that occurred last Thursday. 
 
 Thanks,
 David Kao
